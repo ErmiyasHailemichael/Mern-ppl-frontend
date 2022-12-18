32a61915349062c9249a0dfde785a742c1dbaf62
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2022-12-17 16:05:05 -0800

    Frontend mern people

---
+++ src/components/Main.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import Index from '../Pages/Index'
+import Show from '../Pages/Show'
+
+
+function Main(props) {
+  const [people, setPeople] = useState([])
+  const URL = "http://localhost:3002/people/"
+
+  const getPeople = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    // console.log(data)
+    setPeople(data)
+  }
+
+  const createPeople = async (person) => {
+    // make post request to create people
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
+    })
+    // update list of people
+    getPeople()
+  }
+
+  const updatePeople = async (person, id) => {
+    // make put request to create people
+    await fetch(URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(person),
+    })
+    // update list of people
+    getPeople()
+  }
+
+  const deletePeople = async (id) => {
+    // make delete request to create people
+    await fetch(URL + id, {
+      method: "DELETE",
+    })
+    // update list of people
+    getPeople()
+  }
+
+
+
+  useEffect(()=> {
+    getPeople()
+  }, [])
+
+  return (
+    <main>
+      <Routes>
+        <Route exact 
+        path="/" 
+        element={<Index  people={people} createPeople={createPeople}/>} />
+
+        <Route path="/people/:id" 
+        element={
+        <Show 
+        people={people}
+        updatePeople={updatePeople}
+        deletePeople={deletePeople}/> } />
+      </Routes>
+    </main>
+  )
+}
+
+export default Main
